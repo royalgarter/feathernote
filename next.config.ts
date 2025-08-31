d470f913499fc55e19c54c5a678baa419d987a82
Tung “Djinni” Pham Thanh <royalgarter@gmail.com>
2025-08-31 17:19:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,20 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   webpack: (config, { isServer }) => {
+    if (!isServer) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            "buffer": require.resolve("buffer/"),
+        };
+    }
+     config.plugins.push(
+      new (require('webpack').ProvidePlugin)({
+        Buffer: ['buffer', 'Buffer'],
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
